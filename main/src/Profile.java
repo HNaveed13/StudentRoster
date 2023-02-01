5f1723e4113590c6cc62d09697d74e00a880f77e
HNaveed13 <56270097+HNaveed13@users.noreply.github.com>
2023-01-31 22:44:09 -0500

    Add files via upload

---
+++ main/src/Profile.java
@@ -0,0 +1,5 @@
+public class Profile implements Comparable<Profile> {
+    private String  lname;
+    private String  fname;
+    private Date    dob;
+}
---
+++ main/src/Student.java
@@ -0,0 +1,5 @@
+public class Student implements comparable <Student> {
+    private Profile profile;
+    private Major   major;
+    private int     creditCompleted;
+}
