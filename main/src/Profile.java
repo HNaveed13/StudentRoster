9aef113ef1e662b311b34b715c9a01893f2d2a35
Hamad Naveed <56270097+HNaveed13@users.noreply.github.com>
2023-03-04 16:45:12 -0500

    Delete main/src directory

--- main/src/Profile.java
+++
@@ -1,5 +0,0 @@
-public class Profile implements Comparable<Profile> {
-    private String  lname;
-    private String  fname;
-    private Date    dob;
-}
--- main/src/Student.java
+++
@@ -1,5 +0,0 @@
-public class Student implements comparable <Student> {
-    private Profile profile;
-    private Major   major;
-    private int     creditCompleted;
-}
